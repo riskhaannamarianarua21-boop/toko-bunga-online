aa2a6fcf310f719617e636d50f4e174a4f91d1a2
Gracia <graciasamino1406@gmail.com>
2026-06-03 03:13:32 +0800

    update web

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,13 @@
 <?php
-$conn=mysqli_connect('localhost','root','','toko_online');
-/* check connection */
+$conn = mysqli_connect(
+    "sql205.infinityfree.com",
+    "if0_42081437",
+    "N50q2RH9AF5",
+    "if0_42081437_tokobunga"
+);
+
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-} else{
-    printf("");
 }
 ?>
